430b8e43a1ea29aa1a3cdcdd2e8cbae43eb3fdaf
kishan kathare <kishankathare45@gmail.com>
2023-03-14 20:31:34 +0530

    Navbar Added

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav className="navbar">
+            <h2 className="title">
+                The Book Shop
+            </h2>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
