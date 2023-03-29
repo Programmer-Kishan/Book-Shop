15f0dde23913b381eda978a226c41c7aef20e469
unknown <kishankathare45@gmail.com>
2023-03-29 22:27:40 +0530

    login page navbar done with responsiveness

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
 import Greeting from './Greeting';
+import Button from '../Helper/Button';
 
-import classes from './Navbar.module.css'
+import classes from './Navbar.module.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = (props) => {
 
-  const navbarClass = props.className + " " + classes.navbar;
-
   return (
     <>
-        <nav className={navbarClass}>
+        <nav className={classes.navbar}>
             <h2>
                 The Book Shop
             </h2>
-            {props.isLogin ? <Greeting name={props.greetName} /> : ''}
-            
+            <div className={classes["login-info"]}>
+              {props.isLogin ? <Greeting name={props.greetName} /> : ''}
+              {props.isLogin ? <Button type='button'>
+                                <span>Your Cart </span><FontAwesomeIcon icon={faCartShopping} />
+                              </Button>
+                                : 
+              ''}
+              {props.isLogin ? <Button type="button" className={classes["nav-btn"]}>Logout</Button>: ''}
+            </div>
         </nav>
     </>
   )
